04896883a9dde31e291fe111406531f19ad72acd
cdOut <88325488+cdOut@users.noreply.github.com>
2025-10-24 13:22:42 +0200

    add glm into cmake config

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "backends/imgui_impl_opengl3.h"
 #include "stb_image.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 #include "Shader.h"
