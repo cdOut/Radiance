f861c71a8c81f184df7a20a1407c69605dd77d5c
cdOut <88325488+cdOut@users.noreply.github.com>
2025-10-26 13:53:42 +0100

    add base mesh class for further primitive shapes

---
+++ include/Mesh.h
@@ -0,0 +1,56 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+struct Transform {
+    glm::vec3 position {0.0f};
+    glm::vec3 rotation {0.0f};
+    glm::vec3 scale {1.0f};
+
+    glm::mat4 getModelMatrix() const {
+        glm::mat4 translationMat = glm::translate(glm::mat4(1.0f), position);
+        glm::quat rotationQuat = glm::quat(glm::radians(rotation));
+        glm::mat4 rotationMat = glm::toMat4(rotationQuat);
+        glm::mat4 scaleMat = glm::scale(glm::mat4(1.0f), scale);
+        return translationMat * rotationMat * scaleMat;
+    }
+};
+
+class Mesh {
+    public:
+        Mesh() : VAO(0), VBO(0), EBO(0), color(1.0f) {}
+
+        virtual ~Mesh() {
+            if (VAO) glDeleteVertexArrays(1, &VAO);
+            if (VBO) glDeleteBuffers(1, &VBO);
+            if (EBO) glDeleteBuffers(1, &EBO);
+        }
+
+        virtual void render() const {
+            glBindVertexArray(VAO);
+            if (EBO != 0) {
+                glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+            } else {
+                glDrawArrays(GL_TRIANGLES, 0, vertices.size() / floatsPerVert);
+            }
+            glBindVertexArray(0);
+        }
+
+        Transform& getTransform() { return transform; }
+        glm::vec3& getColor() { return color; }
+    protected:
+        std::vector<float> vertices;
+        unsigned int floatsPerVert;
+        std::vector<unsigned int> indices;
+        unsigned int VAO, VBO, EBO;
+
+        Transform transform;
+        glm::vec3 color;
+};
+
+#endif
