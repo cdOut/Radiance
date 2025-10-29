8e1c9c9b329db0c8713abf9511d01148cf2dbce1
cdOut <88325488+cdOut@users.noreply.github.com>
2025-10-29 18:09:59 +0100

    restructure mesh code

--- include/Mesh.h
+++ include/Mesh.h
@@ -36,6 +36,25 @@ class Mesh {
             if (EBO) glDeleteBuffers(1, &EBO);
         }
 
+        virtual void setupMesh() {
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+
+            glBindVertexArray(VAO);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+            glEnableVertexAttribArray(0);
+
+            glBindVertexArray(0);
+        }
+
         virtual void render(Shader& shader) const {
             shader.use();
 
@@ -53,9 +72,6 @@ class Mesh {
             }
             glBindVertexArray(0);
         }
-
-        Transform& getTransform() { return transform; }
-        glm::vec3& getColor() { return color; }
     protected:
         std::vector<float> vertices;
         unsigned int floatsPerVert;
