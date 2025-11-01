a7ee4dc9a214487a64ec7e91fdfedf0f7a55d2d3
cdOut <88325488+cdOut@users.noreply.github.com>
2025-11-01 13:22:37 +0100

    fix cone indices order for front facing tris

--- include/Cone.h
+++ include/Cone.h
@@ -7,25 +7,26 @@ class Cone : public Mesh {
     public:
         Cone () {
             float r = 0.5f;
+            float height = 1.0f;
             unsigned int segments = 16;
 
             for (size_t i = 0; i < segments; i++) {
                 float angle = 2.0f * M_PI * i / segments;
                 float x = r * cosf(angle);
                 float z = r * sinf(angle);
-                vertices.insert(vertices.end(), {x, -r, z});
+                vertices.insert(vertices.end(), {x, -height * 0.5f, z});
             }
-            vertices.insert(vertices.end(), {0.0f, r, 0.0f});
-            vertices.insert(vertices.end(), {0.0f, -r, 0.0f});
+            vertices.insert(vertices.end(), {0.0f, -height * 0.5f, 0.0f});
+            vertices.insert(vertices.end(), {0.0f, height * 0.5f, 0.0f});
 
             for (unsigned int i = 0; i < segments; i++) {
-                unsigned int ii = (i + 1) % segments;
-                indices.insert(indices.end(), {i, ii, segments});
+                unsigned int next = (i + 1) % segments;
+                indices.insert(indices.end(), {segments, next, i});
             }
 
             for (unsigned int i = 0; i < segments; i++) {
-                unsigned int ii = (i + 1) % segments;
-                indices.insert(indices.end(), {segments + 1, ii, i});
+                unsigned int next = (i + 1) % segments;
+                indices.insert(indices.end(), {i, next, segments + 1});
             }
 
             setupMesh();
